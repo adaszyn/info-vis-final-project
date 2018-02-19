0ff07e5f138a065c372ff58f6c6d6f0a29132327
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-02-19 19:32:06 +0100

    Range changed to TimeRange, state kept in Statistics component

--- src/components/range-picker/RangePicker.js
+++ src/components/range-picker/RangePicker.js
@@ -3,18 +3,9 @@ import InputRange from "react-input-range";
 import "./RangePicker.css";
 
 export class RangePicker extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <InputRange
-        maxValue={this.props.maxValue}
-        minValue={this.props.minValue}
-        value={this.props.value}
-        onChange={this.props.onChange}
-      />
+      <InputRange {...this.props} />
     );
   }
 }
--- src/components/statistics/TimeStatistics.js
+++ src/components/statistics/TimeStatistics.js
@@ -2,20 +2,19 @@ import React, { Component } from "react";
 import { RangePicker } from "../range-picker/RangePicker";
 
 export class TimeStatistics extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value: { min: 10, max: 100 }
-    };
-  }
   onValueChange = value => {
     this.setState({ value });
   };
   render() {
     return (
       <div className="statistics-box">
-        <h2 className="statistics-box__header">TIME</h2>
-        <RangePicker minValue={10} maxValue={100} value={this.state.value} onChange={this.onValueChange} />
+        <h2 className="statistics-box__header">PERIOD</h2>
+        <RangePicker
+            minValue={2005}
+            maxValue={2017}
+            step={1}
+            value={this.props.value}
+            onChange={this.props.onChange} />
       </div>
     );
   }
