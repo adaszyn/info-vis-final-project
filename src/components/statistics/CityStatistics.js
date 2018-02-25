7fae9143bec415d5825ae20ed3d2876a1d647f6f
ranjini <ranjini1992@gmail.com>
2018-02-26 00:42:02 +0100

    changed layout of find my location with new design

--- src/components/statistics/CityStatistics.js
+++ src/components/statistics/CityStatistics.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { BarChart } from "../bar-chart/BarChart";
+import { FindUserLocation } from "../find-user-location/FindUserLocation";
 
 export class CityStatistics extends Component {
   render() {
     return (
       <div className="statistics-box">
-        <h2 className="statistics-box__header">CITY</h2>
+        <h2 className="statistics-box__header">
+        	CITY
+        	<FindUserLocation handleClick={this.props.handleClick} />
+        </h2>
+        
         <BarChart values={this.props.crimesByCity} />
       </div>
     );
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -14,13 +14,13 @@ export class Statistics extends Component {
           data={this.props.crimesByRegion}
           wrappedComponent={RegionStatistics}
           dataLabel="crimesByRegion"
-          handleClick={this.props.handleClick}
           containerClassName="statistics-box-loader-container"
         />
         <LoadingComponent
           data={this.props.crimesByCity}
           wrappedComponent={CityStatistics}
           dataLabel="crimesByCity"
+          handleClick={this.props.handleClick}
           containerClassName="statistics-box-loader-container"
         />
         <LoadingComponent
