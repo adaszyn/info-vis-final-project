d91cad797096add3c4bfacacc18d1a1c792c6199
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-02-25 13:26:24 +0100

    apiBase configured according to NODE_ENV

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,7 @@
 const CONFIG = {
-    apiBase: 'http://localhost:3000'
-}
+  apiBase:
+    process.env.NODE_ENV === 'production'
+      ? 'https://info-vis-api.herokuapp.com'
+      : 'http://localhost:3000',
+};
 export default CONFIG;
