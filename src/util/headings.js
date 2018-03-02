481a4cb2ed59f705c15769b37892c136281520e5
ranjini <ranjini1992@gmail.com>
2018-03-02 11:09:27 +0100

    prettier code

--- src/util/headings.js
+++ src/util/headings.js
@@ -1,9 +1,9 @@
-import { memoize } from "underscore";
 import { headings } from "../constants/headings";
 
 export const getTranslatedHeading = (id, language) => {
-  if (!headings[id]) {
+  const heading = headings[id];
+  if (!heading) {
     return id;
   }
-  return language === "swedish" ? headings[id].name : headings[id].translation;
+  return heading[language]? heading[language]: id;
 };
